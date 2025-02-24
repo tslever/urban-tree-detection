88d6a1049e6d79be42769db32aeb7a89b26a8d35
Tom Lever <thomas.lever.business@gmail.com>
2025-02-23 22:04:10 -0500

    Allow rearranging channels.

--- scripts/test.py
+++ scripts/test.py
@@ -15,7 +15,7 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def save_visualizations(images, results, names, output_dir):
+def save_visualizations(images, results, names, output_dir, rearrange_channels = False):
     '''
     Save one visualization per test image with the test image and overlaid annotations.
 
@@ -24,23 +24,30 @@ def save_visualizations(images, results, names, output_dir):
         results: dict -- Dictionary from function evaluate containing keys `gt_locs`, `tp_locs`, `tp_gt_locs`, `fp_locs`, and `fn_locs`
         names: list -- List of names corresponding to each test image
         output_dir: str -- Path to the directory where visualizations will be saved
+        rearrange_channels: bool -- If True, rearrange channels (for example, use channel order [3, 0, 1] if available)
     '''
     os.makedirs(output_dir, exist_ok = True)
     num_images = images.shape[0]
     for i in range(0, num_images):
         fig, ax = plt.subplots(figsize = (8, 8))
-        ax.imshow(images[i])
+        img = images[i]
+        if rearrange_channels:
+            if img.shape[-1] >= 4:
+                img = img[..., [3, 0, 1]]
+            else:
+                img = img[..., :3]
+        ax.imshow(img)
         ground_truth_points = results['gt_locs'][i]
         if ground_truth_points.size > 0:
             if len(ground_truth_points.shape) == 1:
                 ground_truth_points = ground_truth_points[None, :]
-            ax.plot(ground_truth_points[:,0], ground_truth_points[:,1], 'm.', label = 'Ground Truth')
+            ax.plot(ground_truth_points[:, 0], ground_truth_points[:, 1], 'm.', label = 'Ground Truth')
 
         true_positives = results['tp_locs'][i]
         if true_positives.size > 0:
             if len(true_positives.shape) == 1:
                 true_positives = true_positives[None, :]
-            ax.plot(true_positives[:,0], true_positives[:,1], 'g+', label = 'True Positives')
+            ax.plot(true_positives[:, 0], true_positives[:, 1], 'g+', label = 'True Positives')
 
         false_positives = results['fp_locs'][i]
         if false_positives.size > 0:
@@ -52,11 +59,11 @@ def save_visualizations(images, results, names, output_dir):
         if false_negatives.size > 0:
             if len(false_negatives.shape) == 1:
                 false_negatives = false_negatives[None, :]
-            ax.plot(false_negatives[:,0], false_negatives[:,1], 'm.', markeredgecolor='k', markeredgewidth=1, label='False Negatives')
+            ax.plot(false_negatives[:, 0], false_negatives[:, 1], 'm.', markeredgecolor = 'k', markeredgewidth = 1, label = 'False Negatives')
 
         true_positives_and_ground_truths = results['tp_gt_locs'][i]
         if true_positives.size > 0 and true_positives_and_ground_truths.size > 0:
-            if len(true_positives.shape) == 1:
+            if len(true_positives_and_ground_truths.shape) == 1:
                 true_positives_and_ground_truths = true_positives_and_ground_truths[None, :]
             for (x1, y1), (x2, y2) in zip(true_positives, true_positives_and_ground_truths):
                 ax.plot([x1, x2], [y1, y2], 'y-')
@@ -71,9 +78,10 @@ def save_visualizations(images, results, names, output_dir):
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('data', help='path to data hdf5 file')
-    parser.add_argument('log', help='path to log directory')
-    parser.add_argument('--max_distance', type=float, default=10, help='max distance from ground truth to pred tree (in pixels)')
+    parser.add_argument('data', help = 'path to data hdf5 file')
+    parser.add_argument('log', help = 'path to log directory')
+    parser.add_argument('--max_distance', type = float, default = 10, help = 'max distance from ground truth to pred tree (in pixels)')
+    parser.add_argument('--rearrange_channels', action = 'store_true', help = 'Rearrange image channels for visualization if provided')
 
     args = parser.parse_args()
 
@@ -117,29 +125,29 @@ def main():
 
     print('----- calculating metrics -----')
     results = evaluate(
-        gts=gts,
-        preds=preds,
-        min_distance=min_distance,
-        threshold_rel=threshold_rel,
-        threshold_abs=threshold_abs,
-        max_distance=args.max_distance,
-        return_locs=True
+        gts = gts,
+        preds = preds,
+        min_distance = min_distance,
+        threshold_rel = threshold_rel,
+        threshold_abs = threshold_abs,
+        max_distance = args.max_distance,
+        return_locs = True
     )
 
-    with open(os.path.join(args.log,'results.txt'),'w') as f:
-        f.write('precision: '+str(results['precision'])+'\n')
-        f.write('recall: '+str(results['recall'])+'\n')
-        f.write('fscore: '+str(results['fscore'])+'\n')
-        f.write('rmse [px]: '+str(results['rmse'])+'\n')
+    with open(os.path.join(args.log, 'results.txt'), 'w') as f:
+        f.write('precision: ' + str(results['precision']) + '\n')
+        f.write('recall: ' + str(results['recall']) + '\n')
+        f.write('fscore: ' + str(results['fscore']) + '\n')
+        f.write('rmse [px]: ' + str(results['rmse']) + '\n')
 
     print('------- results for: ' + args.log + ' ---------')
-    print('precision: ',results['precision'])
-    print('recall: ',results['recall'])
-    print('fscore: ',results['fscore'])
-    print('rmse [px]: ',results['rmse'])
+    print('precision: ', results['precision'])
+    print('recall: ', results['recall'])
+    print('fscore: ', results['fscore'])
+    print('rmse [px]: ', results['rmse'])
     
     vis_dir = os.path.join(args.log, 'visualizations')
-    save_visualizations(images, results, names, vis_dir)
+    save_visualizations(images, results, names, vis_dir, rearrange_channels = args.rearrange_channels)
     print('Visualizations saved to directory:', vis_dir)
 
 
