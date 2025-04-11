b3266e48d3b737ad3856b73b867d8b6d3fec97fe
Tom Lever <thomas.lever.business@gmail.com>
2025-04-10 22:59:59 -0400

    Add confidences to inferred geospatial layer.

--- scripts/inference.py
+++ scripts/inference.py
@@ -19,18 +19,17 @@ import glob
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('input', help='path to input tiff file or directory')
-    parser.add_argument('output', help='path to output json file or directory')
-    parser.add_argument('log', help='path to log directory')
-    parser.add_argument('--bands', default='RGBN', help='input bands')
-    parser.add_argument('--tile_size', type=int, default=2048, help='tile size')
-    parser.add_argument('--overlap', type=int, default=32, help='overlap between tiles')
-
+    parser.add_argument('input', help = 'path to input tiff file or directory')
+    parser.add_argument('output', help = 'path to output json file or directory')
+    parser.add_argument('log', help = 'path to log directory')
+    parser.add_argument('--bands', default = 'RGBN', help = 'input bands')
+    parser.add_argument('--tile_size', type = int, default = 2048, help = 'tile size')
+    parser.add_argument('--overlap', type = int, default = 32, help = 'overlap between tiles')
     args = parser.parse_args()
 
-    params_path = os.path.join(args.log,'params.yaml')
+    params_path = os.path.join(args.log, 'params.yaml')
     if os.path.exists(params_path):
-        with open(params_path,'r') as f:
+        with open(params_path, 'r') as f:
             params = yaml.safe_load(f)
             mode = params['mode']
             min_distance = params['min_distance']
@@ -42,23 +41,27 @@ def main():
         threshold_abs = None
         threshold_rel = 0.2
     
-    weights_path = os.path.join(args.log,'best.weights.h5')
-    padded_size = args.tile_size + args.overlap*2
+    weights_path = os.path.join(args.log, 'best.weights.h5')
+    padded_size = args.tile_size + args.overlap * 2
     preprocess = eval(f'preprocess_{args.bands}')
-    training_model, model = SFANet.build_model((padded_size,padded_size,len(args.bands)),preprocess_fn=preprocess)
+    training_model, model = SFANet.build_model((padded_size, padded_size, len(args.bands)), preprocess_fn = preprocess)
     training_model.load_weights(weights_path)
     
     if os.path.isdir(args.input):
-        os.makedirs(args.output,exist_ok=True)
-        paths = sorted(glob.glob(os.path.join(args.input,'*.tif')) + glob.glob(os.path.join(args.input,'*.tiff')))
-        pbar = tqdm.tqdm(total=len(paths))
+        os.makedirs(args.output, exist_ok = True)
+        paths = sorted(glob.glob(os.path.join(args.input, '*.tif')) + glob.glob(os.path.join(args.input, '*.tiff')))
+        pbar = tqdm.tqdm(total = len(paths))
         for input_path in paths:
-            output_path = os.path.join(args.output,os.path.basename(input_path).split('.')[0]+'.json')
+            output_path = os.path.join(args.output, os.path.basename(input_path).split('.')[0] + '.json')
             if not os.path.exists(output_path):
-                run_tiled_inference(model,input_path,output_path,min_distance=min_distance,threshold_abs=threshold_abs,threshold_rel=threshold_rel)
+                run_tiled_inference(
+                    model, input_path, output_path, min_distance = min_distance, threshold_abs = threshold_abs, threshold_rel = threshold_rel
+                )
             pbar.update(1)
     else:
-        run_tiled_inference(model,args.input,args.output,min_distance=min_distance,threshold_abs=threshold_abs,threshold_rel=threshold_rel)
+        run_tiled_inference(
+            model, args.input, args.output, min_distance = min_distance, threshold_abs = threshold_abs, threshold_rel = threshold_rel
+        )
 
 if __name__ == '__main__':
     main()
